cb75897c798be693c8d69810b98a6ef01b78af35
Mauricio Gomes das Chagas <maumauriciog@hotmail.com>
2024-03-29 17:27:58 -0300

    Create new screen. ResultActivity. versão 1.7

--- app/src/main/java/com/example/calculadoraimcv2/ResultActivity.kt
+++ app/src/main/java/com/example/calculadoraimcv2/ResultActivity.kt
@@ -18,10 +18,32 @@ class ResultActivity : AppCompatActivity() {
             insets
         }
         val rIMC = findViewById<TextView>(R.id.txtRespIMC)
+        val rClassfield = findViewById<TextView>(R.id.txtClassf)
+
         val getPeso = intent.getFloatExtra("dateIMCp", 0f)
         val getAltura = intent.getFloatExtra("dateIMCa", 0f)
         val getRespIMC = intent.getFloatExtra("dateIMC", 0f)
 
+        var respClassified: String
+
+        if (getRespIMC <= 16.9){
+            respClassified = "Muito Abaixo do Peso"
+        }else if (getRespIMC >= 17 && getRespIMC <= 18.4) {
+            respClassified = "Abaixo do Peso"
+        } else if (getRespIMC >= 18.5 && getRespIMC <= 24.9){
+            respClassified = "Peso Normal"
+        } else if (getRespIMC >= 25.0 && getRespIMC <= 29.9) {
+            respClassified = "Acima do Peso"
+        } else if (getRespIMC >= 30.0 && getRespIMC <= 34.9) {
+            respClassified = "Obesidade Grau I"
+        }else if (getRespIMC >= 35.0 && getRespIMC <= 40.0) {
+            respClassified = "Obesidade Grau II"
+        } else if (getRespIMC > 40.0) {
+            respClassified = "Obesidade Grau III"
+        }
+
         rIMC.text = getRespIMC.toString()
+        rClassfield.text = respClassified
+
     }
 }
