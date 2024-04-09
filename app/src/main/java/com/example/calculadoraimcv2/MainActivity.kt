9809a036d28c65361083a3a930fdf220d2ea437a
Mauricio Gomes das Chagas <maumauriciog@hotmail.com>
2024-04-09 16:24:52 -0300

    Melhorando a experiência do usuário 3.2

--- app/src/main/java/com/example/calculadoraimcv2/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimcv2/MainActivity.kt
@@ -15,14 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-
 
         val btnButton = findViewById<Button>(R.id.btnIMC)
         val impPeso = findViewById<TextInputEditText>(R.id.impPeso)
